ace904e196f74e80f6807a37ff84556e94f559b0
Devariste <devaristewilkenson@gmail.com>
2025-12-11 12:34:45 -0400

    Redo the note app for the first time

--- index.js
+++ index.js
@@ -1,78 +1,70 @@
-"use strict"
 
+const noteBtn = document.querySelector(".btn");
 const noteContainer = document.querySelector(".note-container");
-const btnAddNotes = document.querySelector(".btn");
 
-btnAddNotes.addEventListener("click", function(e){
-    e.preventDefault();
-    addNotes();
+noteBtn.addEventListener("click", (e)=>{
+    AddNotes();
 });
 
-const saveNotes = function(){
+function SaveNotes(){
+    const titles = document.querySelectorAll(".note .note-title");
+    const contents = document.querySelectorAll(".note .content");
+    
     const data = [];
-    const noteContents  = document.querySelectorAll(".note .content");
-    const noteTitles = document.querySelectorAll(".note .title");
-    console.log(noteContents);
-    console.log(noteTitles);
-
-    noteContents.forEach((note, index) =>{
-        const content = note.value;
-        console.log(content);
-        const title = noteTitles[index].value;
-        if(content.trim() !== ""){
-            data.push({title, content});
+    contents.forEach((content, index) =>{
+        const noteValue = content.value;
+        const title = titles[index].value;
+        if(noteValue.trim() !== ""){
+            data.push({title, noteValue})
         }
     });
 
     const titlesData = data.map((item) => item.title);
     localStorage.setItem("titles", JSON.stringify(titlesData));
 
-    const contentData = data.map((item) => item.content);
-    localStorage.setItem("contents", JSON.stringify(contentData));
-};
-
-
-const addNotes = function(title="", content=""){
-    const note = document.createElement("div");
-    note.classList.add("note");
-    note.innerHTML = `<div class="icons">
-        <i class="save fas fa-save" style="color:red"></i>
-        <i class="trash fas fa-trash" style="color:yellow"></i> 
-    </div>
-    <div class="title-div">
-        <textarea class="title" placeholder="Write a title...">${title}</textarea>
-    </div>
-    <textarea class="content" placeholder="Write a content...">${content}</textarea>
-    `;
-    noteContainer.appendChild(note);
-
-    const save = note.querySelector(".save");
-    const trash = note.querySelector(".trash");
-    saveNotes();
-
-    save.addEventListener("click", function(e){
-        e.preventDefault();
-        saveNotes();
-    });
-
-    trash.addEventListener("click", function(e){
-        e.preventDefault();
-        note.remove();
-        saveNotes();
-    });
+    const contentsData = data.map((item) => item.noteValue);
+    localStorage.setItem("contents", JSON.stringify(contentsData));
 }
 
-function loadData(){
+function AddNotes(title="", content=""){
+    const note = document.createElement("div");
+    note.classList.add("note");
+    note.innerHTML = `
+    <div  class="icons">
+    <i class="fa-solid fa-trash" id="delete-note"></i>
+    <i class="fa-solid fa-floppy-disk" id="save-note"></i>
+    </div>
+    <div class="title-div">
+        <textarea placeholder="Enter a title" class="note-title">${title}</textarea>
+    </div>
+    <textarea class="content" placeholder="Enter your note here">${content}</textarea>
+    `;
+
+   
+    const deleteNote = note.querySelector("#delete-note");
+    const save = note.querySelector("#save-note");
+
+    deleteNote.addEventListener("click", ()=>{
+        note.remove();
+        SaveNotes()
+    });
+
+    save.addEventListener("click", ()=>{
+        SaveNotes();  
+    });
+
+    noteContainer.appendChild(note);
+    SaveNotes();
+}
+
+function loadNotes(){
     const titles = JSON.parse(localStorage.getItem("titles")) || [];
     const contents = JSON.parse(localStorage.getItem("contents")) || [];
 
-    for(let i=0; i< Math.max(titles.length, contents.length); i++){
-        const title = titles[i]?? "(no title)";
-        const content = contents[i]?? "(no content)";
-        addNotes(title, content);
-    };
-} 
+    for(let i=0; i<Math.max(titles.length, contents.length); i++){
+        AddNotes(titles[i], contents[i]);
+    }
+}
 
-loadData();
+loadNotes();
 
-    
